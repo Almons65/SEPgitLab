e0bea093da8d2b593a022c8e0e74a3a3478fff68
PluemSi <singchayapon.k@gmail.com>
2025-12-23 18:27:47 +0700

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -20,16 +20,28 @@ class Walk( Transportation ):
 
    def find_cost( self ):
       return 0
+   
+class Train( Transportation ):
+
+   def __init__( self, start, end, distance ):
+      self.stations = ["Bang Sue", "Chatuchak Park", "Phahon Yothin", "Ladkrabang Station","Payathai Station"]
+      Transportation.__init__( self, start, end, distance)
+
+   def find_cost( self ):
+      base_fare = 35
+      cost_per_km = 5
+      station_cost = 5 * (len(self.stations) - 1)  # Subtracting 1 for the starting station
+      return (cost_per_km * self.distance) + station_cost
 
    
 # main program
 
 travel_cost = 0
 
-#trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
-         #Taxi("KMITL SCB Bank","Ladkrabang Station",5),
-         #Train("Ladkrabang Station","Payathai Station",40,6),
-         #Taxi("Payathai Station","The British Council",3) ]
+trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
+         Taxi("KMITL SCB Bank","Ladkrabang Station",5),
+         Train("Ladkrabang Station","Payathai Station",40,6),
+         Taxi("Payathai Station","The British Council",3) ]
 
 for travel in trip:
    travel_cost += travel.find_cost()
